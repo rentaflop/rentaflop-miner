e4f223b39f4a2896fd4fcf91c5d322fd136b5f02
David Sokol <dasokol@umich.edu>
2022-01-19 20:20:09 -0500

    add logging and other daemon functionality

--- daemon.py
+++ daemon.py
@@ -6,15 +6,53 @@ usage:
     python daemon.py
 """
 import os
+import logging
+
+
+def _get_logger():
+    """
+    modules use this to create/retrieve and configure how logging works for their specific module
+    """
+    name = "daemon"
+    log_path = os.path.dirname(os.path.realpath(__file__))
+    module_logger = logging.getLogger(name)
+    handler = logging.FileHandler(os.path.join(log_path, f"{name}.log"))
+    handler.setLevel(logging.DEBUG)
+    handler.setFormatter(logging.Formatter("%(filename)s:%(lineno)d %(levelname)s %(asctime)s - %(message)s"))
+    module_logger.addHandler(handler)
+    module_logger.setLevel(logging.DEBUG)
+
+    return module_logger
+
+
+DAEMON_LOGGER = _get_logger()
+
+
+def _log_before_after(func, params):
+    """
+    wrapper to log debug info before and after each daemon command
+    """
+    def wrapper():
+        DAEMON_LOGGER.debug(f"Entering {func.__name__} with params {params}...")
+        ret_val = func(params)
+        DAEMON_LOGGER.debug(f"Exiting {func.__name__}.")
+
+        return ret_val
+
+    return wrapper
 
 
 def _run_shell_cmd(cmd):
     """
     run cmd and log output
     """
-    os.system(cmd)
+    DAEMON_LOGGER.debug(f'''Running command {cmd}...''')
+    output = subprocess.check_output(cmd, shell=True, text=True)
+    DAEMON_LOGGER.debug(f'''Command output: {output}''')
 
-    
+    return output
+
+
 def mine(params):
     """
     handle commands related to mining, whether crypto mining or guest "mining"
@@ -39,7 +77,7 @@ def update(params):
         -y --allow-downgrades --allow-remove-essential --allow-change-held-packages \
         dist-upgrade")
 
-    # daemon will shut down so ensure it starts back up again
+    # daemon will shut down so this ensures it starts back up again
     _run_shell_cmd('echo "sleep 5; python3 daemon.py" | at now')
 
     return True
@@ -53,6 +91,9 @@ def uninstall(params):
     # TODO rename images/containers
     _run_shell_cmd('docker stop $(docker ps --filter "name=ssh*" -q)')
     _run_shell_cmd('docker rmi $(docker images -q "dasokol/*") $(docker images "nvidia/cuda" -a -q)')
+    # TODO send logs first
+    # clean up rentaflop host software
+    _run_shell_cmd("rm -rf ../rentaflop-host")
 
     return True
 
@@ -61,7 +102,10 @@ def send_logs(params):
     """
     gather host logs and send back to rentaflop servers
     """
-    pass
+    with open("daemon.log", "r") as f:
+        logs = f.read()
+
+    # TODO send logs to server
 
 
 def main():
@@ -79,7 +123,8 @@ def main():
         params = response["params"]
         if cmd in cmd_to_func:
             # TODO try except here
-            finished = cmd_to_func[cmd](params)
+            func = cmd_to_func[cmd]
+            finished = _log_before_after(func, params)
 
 
 if __name__=="__main__":
