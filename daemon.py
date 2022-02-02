2fe531a6d70a600e6be4567113855f7ed4773420
David Sokol <dasokol@umich.edu>
2022-02-02 15:41:27 -0500

    log crypto mining

--- daemon.py
+++ daemon.py
@@ -226,6 +226,7 @@ def before_request():
     if not request.is_secure:
         url = request.url.replace('http://', 'https://', 1)
         code = 301
+
         return redirect(url, code=code)
 
 
