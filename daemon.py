cf00453eae85b09a91e777a61f9a40db50ae826f
David Sokol <dasokol@umich.edu>
2022-02-13 12:40:38 -0500

    fix format string issue

--- daemon.py
+++ daemon.py
@@ -210,7 +210,8 @@ def mine(params):
         --device /dev/nvidia-modeset:/dev/nvidia-modeset --device /dev/nvidia-uvm:/dev/nvidia-uvm --device /dev/nvidia-uvm-tools:/dev/nvidia-uvm-tools \
         --rm --name {container_name} --env RENTAFLOP_SANDBOX_TYPE={mine_type} --env RENTAFLOP_ID={RENTAFLOP_ID} {gpc_flags} -dt rentaflop/sandbox")
     elif action == "stop":
-        container_name = run_shell_cmd(f'docker ps --filter "name=rentaflop-sandbox-{mine_type}-{gpu}-*" --format {{.Names}}', format_output=False).replace("\n", "")
+        container_name = run_shell_cmd(f'docker ps --filter "name=rentaflop-sandbox-{mine_type}-{gpu}-*"' + \
+                                       ' --format {{.Names}}', format_output=False).replace("\n", "")
         if container_name:
             jupyter_port, ssh_port = container_name.split("-")[-2:]
             run_shell_cmd(f"docker kill {container_name}")
