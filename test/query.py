479153478c9e5bef489ed4d8f6c1d1c1b1739135
David Sokol <dasokol@umich.edu>
2022-03-30 18:46:42 -0400

    add query sender program

---
+++ test/query.py
@@ -0,0 +1,49 @@
+"""
+program for sending test queries to hosts
+"""
+import argparse
+import json
+import requests
+
+
+def parse_clargs():
+    """
+    parse and return command line args
+    """
+    parser = argparse.ArgumentParser(description='Test daemon queries')
+    parser.add_argument('-u', '--url', help="daemon URL including port")
+    parser.add_argument('-r', '--rentaflop_id', help="rentaflop id of host")
+    parser.add_argument('-c', '--cmd', help="command to send host")
+    parser.add_argument('-p', '--params', help="params for command")
+    parser.add_argument('-f', '--file', help="file for command")
+    args = parser.parse_args()
+
+    return args
+
+
+def send_query(args):
+    """
+    construct and send query from args
+    """
+    url = args.url
+    rentaflop_id = args.rentaflop_id
+    cmd = args.cmd
+    params = args.params
+    params = json.loads(params)
+    render_file = args.file
+    data = {"cmd": cmd, "params": params, "rentaflop_id": rentaflop_id}
+    files = {"json": json.dumps(data)}
+    if render_file:
+        files['render_file'] = open(render_file, 'rb')
+    
+    response = requests.post(url, files=files, verify=False)
+    print(response.text)
+
+
+def main():
+    args = parse_clargs()
+    send_query(args)
+
+
+if __name__=="__main__":
+    main()
