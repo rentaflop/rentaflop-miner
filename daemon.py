7f6cf92492cc61e949268fecfcf99e5d6be92674
David Sokol <dasokol@umich.edu>
2022-04-15 23:06:29 -0400

    git rid of multi process checker since daemon normally runs more than one process

--- daemon.py
+++ daemon.py
@@ -133,13 +133,6 @@ def _subsequent_startup():
         # flushing logs and exiting daemon now since it's set to restart in 3 seconds
         logging.shutdown()
         sys.exit(0)
-    else:
-        # if not in the middle of update then we need to exit if another daemon is running
-        # during updates, hive will try to keep restarting the daemon but we don't want this as it'll start itself
-        # run very quietly so as not to pollute last line if we just completed an update
-        running_daemons = run_shell_cmd('ps aux | grep "daemon.py" | grep -v grep', very_quiet=True, format_output=False).splitlines()
-        if len(running_daemons) > 1:
-            sys.exit(0)
 
     # get last line of log file
     with open(LOG_FILE, 'rb') as f:
