d1e5e733abc3179d26b8793b1f07c09c0408e2c7
David Sokol <dasokol@umich.edu>
2022-04-21 15:40:46 -0400

    print full traceback on default catch

--- daemon.py
+++ daemon.py
@@ -24,6 +24,7 @@ import socket
 import urllib3
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 import time
+import traceback
 
 
 app = Flask(__name__)
@@ -471,7 +472,7 @@ def main():
         except KeyboardInterrupt:
             prep_daemon_shutdown(server)
     except Exception as e:
-        DAEMON_LOGGER.error(f"Entering update loop because of uncaught exception: {e}")
+        DAEMON_LOGGER.error(f"Entering update loop because of uncaught exception: {e}\n{traceback.print_exc()}")
         # don't loop too fast
         time.sleep(300)
         # handle runtime errors and other issues by performing an update, preventing most bugs from breaking a rentaflop installation
