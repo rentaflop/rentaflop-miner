8a59dc7c2ac1d560e8a991923cbf26194e69d2cf
David Sokol <dasokol@umich.edu>
2022-06-05 12:36:39 -0400

    find IGD when there's an extra part of matching valid IGD string

--- utils.py
+++ utils.py
@@ -78,16 +78,16 @@ def get_igd():
     timeouts = 10
     time_length = 1
     for _ in range(timeouts):
-        output = run_shell_cmd('upnpc -s | grep "Found valid IGD" | cut -d " " -f 5', format_output=False)
+        output = run_shell_cmd('upnpc -s | grep "Found valid IGD"', format_output=False)
         if "No IGD UPnP Device found" in output:
             time.sleep(time_length)
             time_length *= 2
             continue
 
-        return output.replace("\n", "")
-
-    if "No IGD UPnP Device found" in output:
-        # TODO enter some sort of error state?
+        for word in output.split():
+            if word.startswith("http"):
+                return word
+        
         return None
 
 
