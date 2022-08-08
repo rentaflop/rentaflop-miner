e1b2d3da0c7b39a1d276db34d3d2110511ec315d
David Sokol <dasokol@umich.edu>
2022-08-08 19:16:39 -0400

    migrate to host api for output endpoint

--- sandbox/run.py
+++ sandbox/run.py
@@ -27,11 +27,20 @@ def main():
         # zip and send output dir
         os.system(f"tar -czf {tgz_path} {output}")
         sandbox_id = os.getenv("SANDBOX_ID")
-        server_url = "https://portal.rentaflop.com/api/host/output"
+        server_url = "https://api.rentaflop.com/host/output"
         task_id = os.path.basename(task_dir)
+        # first request to get upload location
         data = {"task_id": str(task_id), "sandbox_id": str(sandbox_id)}
-        files = {'output': open(tgz_path, 'rb'), 'json': json.dumps(data)}
-        requests.post(server_url, files=files)
+        response = requests.post(server_url, json=data)
+        storage_url, fields = response["url"], response["fields"]
+        # upload output to upload location
+        with open(tgz_path, 'rb') as f:
+            files = {'file': f}
+            storage_response = requests.post(storage_url, data=fields, files=files)
+
+        # confirm upload
+        data["confirm"] = True
+        requests.post(server_url, json=data)
         # lets the sandbox queue know when the run is finished
         os.system(f"touch {task_dir}/finished.txt")
 
--- sandbox/sandbox_queue.py
+++ sandbox/sandbox_queue.py
@@ -184,13 +184,12 @@ def _handle_benchmark():
         return
     
     # benchmark job has finished running, so send output and exit container
-    server_url = "https://portal.rentaflop.com/api/host/output"
+    server_url = "https://api.rentaflop.com/host/output"
     benchmark = _read_benchmark()
     sandbox_id = os.getenv("SANDBOX_ID")
     gpu = os.getenv("GPU")
     data = {"benchmark": str(benchmark), "sandbox_id": str(sandbox_id), "gpu": gpu}
-    files = {'json': json.dumps(data)}
-    requests.post(server_url, files=files)
+    requests.post(server_url, json=data)
     # this terminates all sandbox_queue.py processes, which finishes sandbox_setup.sh and the container CMD, causing container exit
     run_shell_cmd('pkill -f "sandbox_queue.py"')
 
