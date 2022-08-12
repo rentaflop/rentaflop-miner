413e4cecd1a33c4c458262570ba26bb390f052f2
David Sokol <dasokol@umich.edu>
2022-08-12 17:20:07 -0400

    ensure we stop docker in the event of a task failure

--- sandbox/run.py
+++ sandbox/run.py
@@ -11,37 +11,41 @@ import json
 
 
 def main():
-    task_dir = sys.argv[1]
-    start_frame = sys.argv[2]
-    end_frame = sys.argv[3]
-    output_path = os.path.join(task_dir, "output/")
-    os.mkdir(output_path)
-    os.system(f"touch {task_dir}/started.txt")
-    # render results for specified frames to output path; disables scripting
-    cmd = f"blender/blender -b {task_dir}/render_file.blend -o {output_path} -s {start_frame} -e {end_frame} --disable-autoexec -a -- --cycles-device OPTIX"
-    return_code = os.system(cmd)
-    # successful render, so send result to servers
-    if return_code == 0:
-        tgz_path = os.path.join(task_dir, "output.tar.gz")
-        output = os.path.join(task_dir, "output")
-        # zip and send output dir
-        os.system(f"tar -czf {tgz_path} {output}")
-        sandbox_id = os.getenv("SANDBOX_ID")
-        server_url = "https://api.rentaflop.com/host/output"
-        task_id = os.path.basename(task_dir)
-        # first request to get upload location
-        data = {"task_id": str(task_id), "sandbox_id": str(sandbox_id)}
-        response = requests.post(server_url, json=data)
-        response_json = response.json()
-        storage_url, fields = response_json["url"], response_json["fields"]
-        # upload output to upload location
-        with open(tgz_path, 'rb') as f:
-            files = {'file': f}
-            storage_response = requests.post(storage_url, data=fields, files=files)
+    try:
+        task_dir = sys.argv[1]
+        start_frame = sys.argv[2]
+        end_frame = sys.argv[3]
+        output_path = os.path.join(task_dir, "output/")
+        os.mkdir(output_path)
+        os.system(f"touch {task_dir}/started.txt")
+        # render results for specified frames to output path; disables scripting
+        cmd = f"blender/blender -b {task_dir}/render_file.blend -o {output_path} -s {start_frame} -e {end_frame} --disable-autoexec -a -- --cycles-device OPTIX"
+        return_code = os.system(cmd)
+        # successful render, so send result to servers
+        if return_code == 0:
+            tgz_path = os.path.join(task_dir, "output.tar.gz")
+            output = os.path.join(task_dir, "output")
+            # zip and send output dir
+            os.system(f"tar -czf {tgz_path} {output}")
+            sandbox_id = os.getenv("SANDBOX_ID")
+            server_url = "https://api.rentaflop.com/host/output"
+            task_id = os.path.basename(task_dir)
+            # first request to get upload location
+            data = {"task_id": str(task_id), "sandbox_id": str(sandbox_id)}
+            response = requests.post(server_url, json=data)
+            response_json = response.json()
+            storage_url, fields = response_json["url"], response_json["fields"]
+            # upload output to upload location
+            with open(tgz_path, 'rb') as f:
+                files = {'file': f}
+                storage_response = requests.post(storage_url, data=fields, files=files)
 
-        # confirm upload
-        data["confirm"] = True
-        requests.post(server_url, json=data)
+            # confirm upload
+            data["confirm"] = True
+            requests.post(server_url, json=data)
+    except:
+        pass
+    finally:
         # lets the sandbox queue know when the run is finished
         os.system(f"touch {task_dir}/finished.txt")
 
