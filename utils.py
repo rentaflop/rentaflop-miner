3e0dac6de29a36093cd8bff854bafdce077091ec
David Sokol <dasokol@umich.edu>
2022-08-17 15:55:06 -0400

    fix version

--- utils.py
+++ utils.py
@@ -175,6 +175,7 @@ def get_state(available_resources, gpu_only=False, quiet=False):
     state["queue"] = []
     khs = 0
     stats = {}
+    rentaflop_version = run_shell_cmd("git rev-parse --short HEAD", quiet=quiet, format_output=False).replace("\n", "")
     # get crypto mining state
     output = run_shell_cmd(f"nvidia-smi", very_quiet=True)
     if "t-rex" in output:
@@ -187,6 +188,7 @@ def get_state(available_resources, gpu_only=False, quiet=False):
     if stats != "null":
         stats["uptime"] = round(time.time() - _START_TIME)
         stats["algo"] = "rentaflop"
+        stats["ver"] = rentaflop_version
     benchmark_container = run_shell_cmd('docker ps --filter "name=rentaflop-benchmark" --filter "ancestor=rentaflop/sandbox" --format {{.Names}}',
                                quiet=quiet, format_output=False).split()
     if benchmark_container:
@@ -216,7 +218,7 @@ def get_state(available_resources, gpu_only=False, quiet=False):
         state["queue"] = container_queue
 
     if not gpu_only:
-        state["version"] = run_shell_cmd("git rev-parse --short HEAD", quiet=quiet, format_output=False).replace("\n", "")
+        state["version"] = rentaflop_version
         state["resources"] = {"gpu_indexes": available_resources["gpu_indexes"]}
         state["khs"] = khs
         state["stats"] = stats
