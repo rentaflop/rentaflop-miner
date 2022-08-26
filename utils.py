ceea8c21b05d9109cafdee443d88d0085f8949ad
David Sokol <dasokol@umich.edu>
2022-08-26 11:02:21 -0400

    fix driver install

--- utils.py
+++ utils.py
@@ -414,8 +414,8 @@ def check_correct_driver(reboot=True):
 
     # ensure opengl files get downloaded for rendering tasks
     run_shell_cmd("sed -e s/--no-opengl-files//g -i /hive/sbin/nvidia-driver-update")
-    # not installed so uninstall existing and install target
-    run_shell_cmd(f"nvidia-driver-update {target_version} --force")
+    # not installed so uninstall existing and install target, running in bg because this command will kill the miner
+    os.system(f"nvidia-driver-update {target_version} --force &")
     if reboot:
         run_shell_cmd("sudo reboot")
 
